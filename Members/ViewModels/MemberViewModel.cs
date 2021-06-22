2a42509212253098ad1fe6dc0d06795ae71e5e80
Dražen L <drazen@altinet.hr>
2021-06-22 20:16:00 +0200

    commit - members list

---
+++ Members/ViewModels/MemberViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Members.ViewModels
+{
+    public class MemberViewModel
+    {
+        public MemberViewModel()
+        {
+        }
+
+        [BindNever]
+        public dynamic ContentItem { get; set; }
+
+    }
+
+}
