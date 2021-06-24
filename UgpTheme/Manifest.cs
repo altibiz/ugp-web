d3be57060ac4586ac73330139c8e175511156742
brunoAltinet <bruno@altinet.hr>
2021-06-24 20:08:28 +0200

    Added UgpTheme

---
+++ UgpTheme/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "UGP Theme",
+    Author = "AltiBiz",
+    Website = "https://www.altibiz.com",
+    Version = "1.0.0",
+    Description = "UGP Theme based on blog theme",
+    Tags = new[] { "Blog", "Bootstrap", "Liquid" }
+)]
