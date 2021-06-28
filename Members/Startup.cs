037cb899b09f191973a4e38e2b1ee37480ef7e2e
brunoAltinet <bruno@altinet.hr>
2021-06-28 14:52:58 +0200

    Latest with shape tracing

--- Members/Startup.cs
+++ Members/Startup.cs
@@ -1,19 +1,32 @@
 using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Data.Migration;
-using OrchardCore.Modules;
 using OrchardCore.Navigation;
+using Microsoft.Extensions.Hosting;
+using OrchardCore.DisplayManagement.Implementation;
+using Members.Utils;
 
 namespace Members
 {
-    public class Startup : StartupBase
+    public class Startup : OrchardCore.Modules.StartupBase
     {
+        public IWebHostEnvironment CurrentEnvironment { get; }
+
+        public Startup(IWebHostEnvironment env)
+        {
+            CurrentEnvironment = env;
+        }
+
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<INavigationProvider, AdminMenu>();
             services.AddScoped<IDataMigration, Migrations>();
+            if(CurrentEnvironment.IsDevelopment())
+                services.AddScoped<IShapeDisplayEvents, ShapeTracingShapeEvents>();
+
         }
 
         public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
