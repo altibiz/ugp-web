ed19033bfda88710b71d7e6276ef678ad716f206
brunoAltinet <bruno@altinet.hr>
2021-07-13 02:16:14 +0200

    Added homepage, carousel and recent news widget (first stab)

---
+++ Carousel.OrchardCore/Manifest.cs
@@ -0,0 +1,11 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Carousel Widget",
+    Author = "David Hayden",
+    Website = "https://www.davidhayden.me",
+    Version = "1.0.0-rc2",
+    Description = "Adds a Bootstrap 4 Carousel Widget.",
+    Dependencies = new[] {"OrchardCore.ContentFields", "OrchardCore.Flows", "OrchardCore.Media", "OrchardCore.Widgets"},
+    Category = "Content"
+)]
