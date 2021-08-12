77b7b1a06018b96e55bfb0c3bebc493f3ca39290
Dražen L <drazen@altinet.hr>
2021-08-12 16:02:34 +0200

    company edit

--- Members/Pages/MyCompany.cshtml.cs
+++ Members/Pages/MyCompany.cshtml.cs
@@ -5,6 +5,10 @@ using YesSql;
 using OrchardCore;
 using OrchardCore.ContentManagement.Display;
 using OrchardCore.DisplayManagement.ModelBinding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+using OrchardCore.DisplayManagement.Notify;
+using Members.Core;
 
 namespace Members.Pages
 {
@@ -15,14 +19,23 @@ namespace Members.Pages
 
         private readonly IUpdateModelAccessor _updateModelAccessor;
 
+        private readonly IHtmlLocalizer H;
+        private readonly MemberService _memberService;
+        private readonly INotifier _notifier;
+
         public dynamic Shape { get; set; }
 
-        public MyCompanyModel(IContentManager contentManager, IContentItemDisplayManager contentItemDisplayManager, IUpdateModelAccessor updateModelAccessor)
+        public MyCompanyModel(IContentManager contentManager, IContentItemDisplayManager contentItemDisplayManager, IUpdateModelAccessor updateModelAccessor,
+            MemberService mService, IHtmlLocalizer<CreateMemberModel> htmlLocalizer, INotifier notifier)
         {
             _contentManager = contentManager;
             _contentItemDisplayManager = contentItemDisplayManager;
 
             _updateModelAccessor = updateModelAccessor;
+
+            _notifier = notifier;
+            H = htmlLocalizer;
+            _memberService = mService;
         }
 
 
@@ -32,6 +45,31 @@ namespace Members.Pages
 
             Shape = await _contentItemDisplayManager.BuildEditorAsync(company, _updateModelAccessor.ModelUpdater, true);
 
+        }
+
+        public async Task<IActionResult> OnPostUpdateCompanyAsync()
+        {
+            return await UpdatePOST("MyCompany");
+        }
+
+        private async Task<IActionResult> UpdatePOST(string nextPage)
+        {
+            ContentItem contentItem;
+            (contentItem, Shape) = await _memberService.GetUpdatedItem(ContentType.Company);
+
+            if (ModelState.IsValid)
+            {
+                var result = await _memberService.UpdateMemberCompany(contentItem);
+
+                if (result.Succeeded)
+                {
+                    _notifier.Success(H["Company updated successful"]);
+                    return RedirectToPage(nextPage+"?companyID="+contentItem.ContentItemId);
+                }
+            }
+            Shape = await _contentItemDisplayManager.BuildEditorAsync(contentItem, _updateModelAccessor.ModelUpdater, true);
+
+            return Page();
         }
     }
 }
