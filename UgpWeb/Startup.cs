1eeb7040b9acdd153f665426c1987738ff44bb2b
brunoAltinet <bruno@altinet.hr>
2021-09-02 18:14:33 +0200

    feat/#38

--- UgpWeb/Startup.cs
+++ UgpWeb/Startup.cs
@@ -16,7 +16,10 @@ namespace UgpWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOrchardCms();
+            services.AddOrchardCms()
+#if DEBUG
+                .AddSetupFeatures("OrchardCore.AutoSetup");
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
