329e362795c13ba3a5cb22be56b20bc2467e869b
Dražen L <drazen@altinet.hr>
2021-09-29 15:28:19 +0200

    feat/#50 - dorada - prikazuje ime parenta

--- Members/Base/ContainedPartDisplayDriver.cs
+++ Members/Base/ContainedPartDisplayDriver.cs
@@ -6,6 +6,7 @@ using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.DisplayManagement.Views;
 using OrchardCore.Lists.Models;
+using System.Threading.Tasks;
 
 namespace Members.Base
 {
@@ -18,20 +19,39 @@ namespace Members.Base
     public class ContainedPartDisplayDriver : ContentDisplayDriver
     {
         private IHttpContextAccessor _httpCA;
-        public ContainedPartDisplayDriver(IHttpContextAccessor httpContextAccessor, MemberService memberService)
+
+        private readonly IContentManager _contentManager;
+
+        public ContentItem MemberContentItem { get; set; }
+        public ContainedPartDisplayDriver(IHttpContextAccessor httpContextAccessor, IContentManager cman)
         {
             _httpCA = httpContextAccessor;
+            _contentManager = cman;
         }
 
-        public override IDisplayResult Edit(ContentItem item)
+        //public override IDisplayResult Edit(ContentItem item)
+        //{
+
+        //    if (!AdminAttribute.IsApplied(_httpCA.HttpContext)) return null;
+
+        //    var part = item.As<ContainedPart>();
+        //    if (part == null) return null;
+
+        //    return Initialize<CpVm>("ContainedPart_Nav", m => { m.ListContentItemId = part.ListContentItemId; })
+        //    .Location("Content");
+        //}
+
+        public override async Task<IDisplayResult> EditAsync(ContentItem model, BuildEditorContext context)
         {
             if (!AdminAttribute.IsApplied(_httpCA.HttpContext)) return null;
-            
-            var part = item.As<ContainedPart>();
+
+            var part = model.As<ContainedPart>();
             if (part == null) return null;
 
-            return Initialize<CpVm>("ContainedPart_Nav", m => { m.ListContentItemId = part.ListContentItemId; })
-            .Location("Content");
+            MemberContentItem = await _contentManager.GetAsync(part.ListContentItemId);
+
+            return Initialize<CpVm>("ContainedPart_Nav", m => { m.ListContentItemId = part.ListContentItemId; m.ParentName = MemberContentItem.DisplayText; }).Location("Content");
+
         }
     }
 }
