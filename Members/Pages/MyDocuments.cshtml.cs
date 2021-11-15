ff0ee3d4c5e548c93ad3303630f255d5ac8353ef
brunoAltinet <bruno@altinet.hr>
2021-11-15 02:30:18 +0100

    Translations

--- Members/Pages/MyDocuments.cshtml.cs
+++ Members/Pages/MyDocuments.cshtml.cs
@@ -12,7 +12,7 @@ namespace Members.Pages
         private readonly IHtmlLocalizer H;
         private readonly MemberService _memberService;
         public Dictionary<string, List<(string, string)>> DocLinks { get; } = new();
-        public MyDocumentsModel(MemberService mService, IHtmlLocalizer<CreateMemberModel> htmlLocalizer, INotifier notifier)
+        public MyDocumentsModel(MemberService mService, IHtmlLocalizer<MyDocumentsModel> htmlLocalizer, INotifier notifier)
         {
             H = htmlLocalizer;
             _memberService = mService;
