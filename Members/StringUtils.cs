328673c677fd158989679b3189c7933435d39bca
brunoAltinet <bruno@altinet.hr>
2025-08-04 14:43:28 +0200

    fixed note

---
+++ Members/StringUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Members
+{
+    public class StringUtils
+    {
+        public static string StripDiacritics(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            var normalized = input.Normalize(NormalizationForm.FormD);
+            var sb = new StringBuilder();
+
+            foreach (var c in normalized)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
